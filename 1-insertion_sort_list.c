3c1535a60af5f44015eb824086a6107dfaddb603
Motunrayo <ajirotutu4real@gmail.com>
2023-09-25 19:01:56 +0000

    Mofied insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,7 +14,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *tmp, *ptr;
 	int swapped = 0;
 
-	if (!list || !(*list))
+	if (!list || !(*list) || (*list)->next == NULL)
 		return;
 	tmp = *list;
 	ptr = *list;
