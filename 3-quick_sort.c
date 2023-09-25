9b9d0aaf748fa74d0e475a54718e4e3eb05ffc6f
Motunrayo <ajirotutu4real@gmail.com>
2023-09-25 18:29:20 +0000

    Added quick_sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sort an array using the quick sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size <= 1)
+		return;
+
+	
+}
